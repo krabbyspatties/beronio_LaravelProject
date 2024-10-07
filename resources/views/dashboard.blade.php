e2cfeaa49d411198a70332e527aa38674e4bcf05
keanne beronio <orcaadmirer@gmial.com>
2024-10-07 18:29:00 +0800

    updated dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,13 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                <div class="p-6 text-gray-900">
+                    <h3 class="text font-bolder">
+                        Hello Keanne
+                    </h3>         
+                    <p class = "bg-blue-400 p-8 rounded-lg">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sagittis nisl et ultricies vulputate. Fusce sodales vehicula vulputate. Suspendisse posuere urna quis sem tristique facilisis. Proin ornare arcu odio, ut sagittis odio laoreet sed. Mauris vulputate eleifend risus lacinia viverra. Duis laoreet, augue id blandit vestibulum, felis est molestie felis, eu egestas diam ex sit amet enim. Vivamus leo mauris, fringilla eget nisi quis, rutrum ultrices sem. Maecenas euismod rutrum congue. Aenean sollicitudin magna ut orci vehicula sagittis. Vestibulum vitae cursus lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis varius convallis ex et faucibus. Vestibulum eu ornare justo.
+                    </p>           
                 </div>
             </div>
         </div>
